2c7994bddbb0f5989278de5571a3abd2b0fa8c9e
Chris G <cwguan@ucsd.edu>
2016-08-25 14:43:39 -0700

    WIP. Working on workout function

---
+++ workout.py
@@ -0,0 +1,10 @@
+def workout():
+    bis_result =
+    tri_result =
+    delts_result =
+    pecs_result =
+    lats_result =
+    core_result =
+    quads_result =
+    hammies_result =
+    workout_result = workout(bis_result, tris_result, delts_result, pecs_result, lats_result, core_result, quads_result, hammies_result)
