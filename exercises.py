e9030f2c2b94054a1e91e42d16d6090efa322fcd
AndrewEToh <aetoh@ucsd.edu>
2016-08-25 10:29:18 -0700

    basic datasets

---
+++ exercises.py
@@ -0,0 +1,12 @@
+#Exercises
+
+biceps = ["barbell curls", "alternating dumbbell curls", "hammer curls", "zottman curls", "spider curls"]
+triceps= ["tricep dips", "skullcrushers", "overhead dumbbell extensions", "tricep pushdowns"]
+shoulders = ["barbell overhead press", "dumbbell overhead press", "dumbbell front/side raises", "barbell shoulder rows", "dumbbell shoulder rows"]
+chest = ["barbell bench press", "dumbbell fly", "decline push-ups", "cable chest crossover"]
+back = ["pull-ups", "bent-over barbell row", "lat pulldowns", "one arm dumbbell row", "t-bar row", "back extension"]
+core = ["planks", "russian twists", "bicycles", "windshield wipers", "v-ups", "side v-ups", "medicine ball throws", "mountain climbers"]
+quads = ["barbell squat", "lunges", "wall-sits", "goblet squat", "box jumps"]
+hams = ["barbell deadlift", "lying leg curls", "single-leg deadlift", "step-ups", "floor glute-ham raise"]
+
+exercises = [biceps, triceps, shoulders, chest, back, core, quads, hams]
