1d67bfe37d231eeb57996b88dc0042f1fd2c2723
cwguan <cwguan@ucsd.edu>
2016-08-26 14:21:46 -0700

    Update render.py

--- render.py
+++ render.py
@@ -46,6 +46,42 @@ def render_search():
     return render_template('search.html')
     
 
+import random
+
+biceps = ["barbell curls", "alternating dumbbell curls", "hammer curls", "zottman curls", "spider curls"]
+triceps= ["tricep dips", "skullcrushers", "overhead dumbbell extensions", "tricep pushdowns"]
+shoulders = ["barbell overhead press", "dumbbell overhead press", "dumbbell front/side raises", "barbell shoulder rows", "dumbbell shoulder rows"]
+chest = ["barbell bench press", "dumbbell fly", "decline push-ups", "cable chest crossover"]
+back = ["pull-ups", "bent-over barbell row", "lat pulldowns", "one arm dumbbell row", "t-bar row", "back extension"]
+core = ["planks", "russian twists", "bicycles", "windshield wipers", "v-ups", "side v-ups", "medicine ball throws", "mountain climbers"]
+quadriceps = ["barbell squat", "lunges", "wall-sits", "goblet squat", "box jumps"]
+hamstrings = ["barbell deadlift", "lying leg curls", "single-leg deadlift", "step-ups", "floor glute-ham raise"]
+
+
+def randomExercise(list):
+    return random.choice(list)
+
+
+def workout(bis_result, tris_result, delts_result, pecs_result, lats_result, core_result, quads_result, hammies_result):
+    your_workout = []
+    if bis_result == True:
+        your_workout.append(randomExercise(biceps))
+    if tris_result == True:
+        your_workout.append(randomExercise(triceps))
+    if delts_result == True:
+        your_workout.append(randomExercise(shoulders))
+    if pecs_result == True:
+        your_workout.append(randomExercise(chest))
+    if lats_result == True:
+        your_workout.append(randomExercise(back))
+    if core_result == True:
+        your_workout.append(randomExercise(core))
+    if quads_result == True:
+        your_workout.append(randomExercise(quadriceps))
+    if hammies_result == True:
+        your_workout.append(randomExercise(hamstrings))
+    return your_workout
+
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
