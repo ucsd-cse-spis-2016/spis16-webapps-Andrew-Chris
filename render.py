7adbbf1b0f5d18bcc9536a13ead5862350bc38f5
cwguan <cwguan@ucsd.edu>
2016-08-30 11:14:30 -0700

    Update render.py

--- render.py
+++ render.py
@@ -53,6 +53,12 @@ exercise_2_link['medicine ball throws'] = 'https://www.youtube.com/watch?v=5lDV_
 exercise_2_link['mountain climbers'] = 'http://hiitacademy.com/wp-content/uploads/2015/02/mountain_climbers.jpg'
 
 #Quadriceps
+exercise_2_link['barbell squat'] = 'http://fitover40challenge.com/wp-content/uploads/2015/04/14294333074669-Squat.jpg'
+exercise_2_link['lunges'] = 'http://assets2.tribesports.com/system/guide_photos/images/000/001/811/large/20130423155649-reverse-dumbbell-lunge.jpg?1366729007'
+exercise_2_link['wall-sits'] = 'http://workoutlabs.com/wp-content/uploads/watermarked/Wall_Sit_squat_M_WorkoutLabs.png'
+exercise_2_link['goblet squats'] = 'http://www.directlyfitness.com/store/wp-content/uploads/2012/09/goblet-squat.jpg'
+exercise_2_link['box jumps'] = 'http://www.outsideonline.com/sites/default/files/styles/three-quarter-page-scaled-1x/public/migrated-images/December2011_BoxJumps_11142011_Featured.jpg?itok=miihcTWH'
+
 #Hamstrings
 exercise_2_link['barbell deadlift'] = 'https://www.youtube.com/watch?v=RovZFcTK_Gg'
 exercise_2_link['lying leg curls'] = 'https://www.youtube.com/watch?v=RovZFcTK_Gg'
