f3c11440543fd34082aecec86412d2855d3d3fae
cwguan <cwguan@ucsd.edu>
2016-09-01 09:33:47 -0700

    Update render.py

--- render.py
+++ render.py
@@ -146,18 +146,6 @@ def render_muscle_group_checklist():
     return render_template('muscle-group-checklist.html')
 
 
-
-import random
-#lists of each exercise for each muscle group 
-biceps = ["barbell curls", "alternating dumbbell curls", "hammer curls", "zottman curls", "spider curls"]
-triceps= ["tricep dips", "skullcrushers", "overhead dumbbell extensions", "tricep pushdowns"]
-shoulders = ["barbell overhead press", "dumbbell overhead press", "dumbbell front/side raises", "barbell shoulder rows", "dumbbell shoulder rows"]
-chest = ["barbell bench press", "dumbbell fly", "decline push-ups", "cable chest crossover"]
-back = ["pull-ups", "bent-over barbell row", "lat pulldowns", "one arm dumbbell row", "t-bar row", "back extension"]
-core = ["planks", "russian twists", "bicycles", "windshield wipers", "v-ups", "side v-ups", "medicine ball throws", "mountain climbers"]
-quadriceps = ["barbell squat", "lunges", "wall-sits", "goblet squat", "box jumps"]
-hamstrings = ["barbell deadlift", "lying leg curls", "single-leg deadlift", "step-ups", "floor glute-ham raise"]
-
 #chooses a random exercise from a certain list
 def randomExercise(list):
     return random.choice(list)
