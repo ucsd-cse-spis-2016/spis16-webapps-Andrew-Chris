f9f00cbaa7c6f7ffecb3bb95b5255475f92dde74
cwguan <cwguan@ucsd.edu>
2016-09-01 09:37:51 -0700

    Update render.py

--- render.py
+++ render.py
@@ -156,8 +156,8 @@ shoulders = ["barbell overhead press", "dumbbell overhead press", "dumbbell fron
 chest = ["barbell bench press", "dumbbell fly", "decline push-ups", "cable chest crossover"]
 back = ["pull-ups", "bent-over barbell row", "lat pulldowns", "one arm dumbbell row", "t-bar row", "back extension"]
 core = ["planks", "russian twists", "bicycles", "windshield wipers", "v-ups", "side v-ups", "medicine ball throws", "mountain climbers"]
-quads = ["barbell squat", "lunges", "wall-sits", "goblet squat", "box jumps"]
-hams = ["barbell deadlift", "lying leg curls", "single-leg deadlift", "step-ups", "floor glute-ham raise"]
+quadriceps = ["barbell squat", "lunges", "wall-sits", "goblet squat", "box jumps"]
+hamstrings = ["barbell deadlift", "lying leg curls", "single-leg deadlift", "step-ups", "floor glute-ham raise"]
 
 #chooses a random exercise from a certain list
 def randomExercise(list):
