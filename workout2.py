815ddd9895491da86a5b04ffa03f5e84f7675a78
AndrewEToh <aetoh@ucsd.edu>
2016-08-31 20:47:27 -0700

    Update workout2.py

--- workout2.py
+++ workout2.py
@@ -81,3 +81,29 @@ core = ["planks", "russian twists", "bicycles", "windshield wipers", "v-ups", "s
 quadriceps = ["barbell squat", "lunges", "wall-sits", "goblet squat", "box jumps"]
 hamstrings = ["barbell deadlift", "lying leg curls", "single-leg deadlift", "step-ups", "floor glute-ham raise"]
 
+
+
+#chooses a random exercise from a certain list
+def randomExercise(list):
+    return random.choice(list)
+
+#checks True/False if user chose a muscle group & adds appropriate exercises into the new list your_workout
+def workout(bis_result, tris_result, delts_result, pecs_result, lats_result, core_result, quads_result, hammies_result):
+    your_workout = []
+    if bis_result == True:
+        your_workout.append(randomExercise(biceps))
+    if tris_result == True:
+        your_workout.append(randomExercise(triceps))
+    if delts_result == True:
+        your_workout.append(randomExercise(shoulders))
+    if pecs_result == True:
+        your_workout.append(randomExercise(chest))
+    if lats_result == True:
+        your_workout.append(randomExercise(back))
+    if core_result == True:
+        your_workout.append(randomExercise(core))
+    if quads_result == True:
+        your_workout.append(randomExercise(quadriceps))
+    if hammies_result == True:
+        your_workout.append(randomExercise(hamstrings))
+    return your_workout
